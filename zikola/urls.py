c0be16ac341493b9f568199a7985d7ce89e4b4b8
wahyuhidayat <muhammadwahyuhidayatmm1@gmail.com>
2024-12-17 17:42:02 +0800

    menambahkan tampilan homepage

--- zikola/urls.py
+++ zikola/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from zikola import views
 
 urlpatterns = [
+    path("", views.index, name="main"),
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls', namespace="courses")),
     path('tasks/', include('tasks.urls', namespace="tasks")),
